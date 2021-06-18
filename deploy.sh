ad0d52c2e2dae15026cb50dd8dcebaeec204da35
Theodore R. Smith <hopeseekr@gmail.com>
2021-06-17 19:59:26 -0600

    Last minute changes for v1.0.

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rsync -rcv --delete-after README.md ZPM/About ZPM/Defs ZPM/Patches ZPM/Textures /c/RimWorld1-2-2900Win64/Mods/ZPM/
+unix2dos /c/RimWorld1-2-2900Win64/Mods/ZPM/README.md
+
+# Remove all of the extra assemblies.
+find /c/RimWorld1-2-2900Win64/Mods/ZPM/Assemblies ! -name 'ZPM.dll' -type f -exec rm -vf {} +
