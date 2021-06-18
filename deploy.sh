e8329e9f28f140f2062209b846f9069a1c406433
Theodore R. Smith <hopeseekr@gmail.com>
2021-06-17 23:59:34 -0600

    I need to figure out how to have mod-dependent research.

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,6 @@ unix2dos /c/RimWorld1-2-2900Win64/Mods/ZPM/README.md
 
 # Remove all of the extra assemblies.
 find /c/RimWorld1-2-2900Win64/Mods/ZPM/Assemblies ! -name 'ZPM.dll' -type f -exec rm -vf {} +
+
+
+rsync -rcv --delete-after /c/RimWorld1-2-2900Win64/Mods/ZPM "/c/Program Files (x86)/Steam/steamapps/common/RimWorld/Mods/"
