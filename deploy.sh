ceb9b08a2f2ca9141c476ebb5c0c0cbd6ffa23a8
Theodore R. Smith <theodore@phpexperts.pro>
2022-01-29 15:01:55 -0600

    Ported to the Linux build system for Rimworld mods.

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-rsync -rcv --delete-after README.md ZPM/About ZPM/Defs ZPM/Patches ZPM/Textures /c/RimWorld1-2-2900Win64/Mods/ZPM/
-unix2dos /c/RimWorld1-2-2900Win64/Mods/ZPM/README.md
+rsync -rcv --delete-after README.md ZPM/About ZPM/Defs ZPM/Patches ZPM/Textures /rimworld/1.2/Mods/ZPM/
+unix2dos /rimworld/1.2/Mods/ZPM/README.md
 
-# Remove all of the extra assemblies.
-find /c/RimWorld1-2-2900Win64/Mods/ZPM/Assemblies ! -name 'ZPM.dll' -type f -exec rm -vf {} +
+rsync -rcv --delete-after README.md ZPM/About ZPM/Defs ZPM/Patches ZPM/Textures /rimworld/1.3/Mods/ZPM/
+unix2dos /rimworld/1.3/Mods/ZPM/README.md
 
-
-rsync -rcv --delete-after /c/RimWorld1-2-2900Win64/Mods/ZPM "/c/Program Files (x86)/Steam/steamapps/common/RimWorld/Mods/"
