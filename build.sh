0ab82c1cb922c36b38d7bb296502fe9a37e5e245
Theodore R. Smith <hopeseekr@gmail.com>
2025-07-07 20:35:24 -0500

    Improved the building efficiency.

--- build.sh
+++ build.sh
@@ -15,21 +15,13 @@ MOD=$(basename $PWD)
 solutionPath="Source/${MOD}.sln"
 
 # Define an array of configurations
-configurations=("Release v1.2" "Release v1.3" "Release v1.4" "Release v1.5" "Release v1.6")
+configurations=("v1.2" "v1.3" "v1.4" "v1.5" "v1.6")
 
-function build() {
-    rm -rf /rimworld/1.2/Mods/${MOD}
+dotnet restore "$solutionPath"
 
-    # Loop through each configuration and build it
-    for config in "${configurations[@]}"; do
-        echo "Building for configuration: $config"
-        dotnet build "$solutionPath" --configuration "$config" &
-    done
-
-    wait  # Blocks until all background jobs finish
-
+function sync_mod() {
     # Copy over the mod directory.
-    cp -af ${MOD} /rimworld/1.2/Mods/
+    rsync -a ${MOD} /rimworld/1.2/Mods/
 
     # Copy over and reformat the README.
     cp README.md /rimworld/1.2/Mods/${MOD}
@@ -38,12 +30,28 @@ function build() {
     rm -rf /rimworld/1.3/Mods/${MOD}
     rm -rf /rimworld/1.4/Mods/${MOD}
     rm -rf /rimworld/1.5/Mods/${MOD}
+    rm -rf /rimworld/1.6/Mods/${MOD}
     rm -rf /rimworld/1.6-steam/Mods/${MOD}
 
     cp -af /rimworld/1.2/Mods/${MOD} /rimworld/1.3/Mods
     cp -af /rimworld/1.2/Mods/${MOD} /rimworld/1.4/Mods
     cp -af /rimworld/1.2/Mods/${MOD} /rimworld/1.5/Mods
+    cp -af /rimworld/1.2/Mods/${MOD} /rimworld/1.6/Mods
     cp -af /rimworld/1.2/Mods/${MOD} /rimworld/1.6-steam/Mods
+}
+
+function build() {
+    rm -rf /rimworld/1.2/Mods/${MOD}
+
+    # Loop through each configuration and build it
+    for config in "${configurations[@]}"; do
+        echo "Building for configuration: $config"
+        dotnet build --no-restore "$solutionPath" --configuration "Release $config" &
+    done
+
+    wait  # Blocks until all background jobs finish
+
+    sync_mod
 
     echo "All builds completed!"
 }
@@ -55,12 +63,16 @@ if [ "$1" == "1" ]; then
     exit
 fi
 
-# Watch for changes to .cs files in the directory and subdirectories
+# Watch for changes to .cs and XML files in the directory and subdirectories
 inotifywait --recursive --monitor --format "%e %w%f" \
-    --event modify,move,create,delete $dir \
-    --include '\.cs$' |
-    while read changed; do
-        echo "Detected change in $changed"
-        build
+    --exclude '/\.idea($|/)' \
+    --event modify,move,create,delete "$dir" "$MOD" |
+    while read event fullpath; do
+        if [[ "$fullpath" == "$dir"* && "$fullpath" == *.cs ]]; then
+            echo "Running build for $fullpath"
+            build
+        elif [[ "$fullpath" == "$MOD"* && "$fullpath" == *.xml ]]; then
+            echo "Running sync_mod for $fullpath"
+            sync_mod
+        fi
     done
-
