271794ae15a029b492d20e6dcb9e84470363b91d
Theodore R. Smith <hopeseekr@gmail.com>
2025-07-23 06:15:32 -0500

    Ported to C# v10.

--- Source/Building_ZPM.cs
+++ Source/Building_ZPM.cs
@@ -1,7 +1,7 @@
 /*
  * This file is part of Uplifted Animals, a Better Rimworlds Project.
  *
- * Copyright © 2021-2024 Theodore R. Smith
+ * Copyright © 2021-2025 Theodore R. Smith
  * Author: Theodore R. Smith <hopeseekr@gmail.com>
  *   GPG Fingerprint: D8EA 6E4D 5952 159D 7759  2BB4 EEB6 CE72 F441 EC41
  *   https://github.com/BetterRimworlds/ZPM
@@ -10,141 +10,130 @@
  * Most rights are reserved.
  */
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using Verse;
-using UnityEngine;
 using RimWorld;
+using UnityEngine;
+using Verse;
 
-namespace BetterRimworlds.ZPM
+namespace BetterRimworlds.ZPM;
+
+[StaticConstructorOnStartup]
+class Building_ZPM : Building
 {
-    [StaticConstructorOnStartup]
-    class Building_ZPM : Building
+    const int ADDITION_DISTANCE = 3;
+
+    private static Dictionary<string, Graphic> chargeGraphics = new Dictionary<string, Graphic>();
+
+    CompPowerBattery power;
+
+    private int darkEnergyReserve = 7500;
+    private int maxDarkEnergy = -1;
+
+    protected Map currentMap;
+
+    static Building_ZPM()
     {
-
-        #region Constants
-
-        const int ADDITION_DISTANCE = 3;
-
-        #endregion
-
-        #region Variables
-
-        private static Dictionary<string, Graphic> chargeGraphics = new Dictionary<string, Graphic>();
-
-        CompPowerBattery power;
-
-        private int darkEnergyReserve = 7500;
-        private int maxDarkEnergy = -1;
-
-        protected Map currentMap;
-
-        #endregion
-
-        static Building_ZPM()
+        if (Building_ZPM.chargeGraphics.Any())
         {
-            if (Building_ZPM.chargeGraphics.Any())
-            {
-                return;
-            }
-
-            string[] powerStates = { "Depleted", "25%", "50%", "75%", "Full" };
-            foreach (var powerState in powerStates)
-            {
-                var graphic = new Graphic_Single();
-#if RIMWORLD12
-                GraphicRequest request = new GraphicRequest(Type.GetType("Graphic_Single"),
-                    $"Things/Buildings/ZPM-{powerState}", ShaderDatabase.DefaultShader, new Vector2(1, 2), Color.white,
-                    Color.white, new GraphicData(), 0, null);
-#else
-                GraphicRequest request = new GraphicRequest(Type.GetType("Graphic_Single"),
-                    $"Things/Buildings/ZPM-{powerState}", ShaderDatabase.DefaultShader, new Vector2(1, 2), Color.white,
-                    Color.white, new GraphicData(), 0, null, null);
-#endif
-                graphic.Init(request);
-                chargeGraphics.Add(powerState, graphic);
-            }
+            return;
         }
 
-        #region Override
-
-        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        string[] powerStates = { "Depleted", "25%", "50%", "75%", "Full" };
+        foreach (var powerState in powerStates)
         {
-            this.currentMap = map;
-            base.SpawnSetup(map, respawningAfterLoad);
-
-            this.power = base.GetComp<CompPowerBattery>();
-            this.maxDarkEnergy = (int) Math.Ceiling(this.power.Props.storedEnergyMax * 1.25);
+            var graphic = new Graphic_Single();
+        #if RIMWORLD12
+            GraphicRequest request = new GraphicRequest(Type.GetType("Graphic_Single"),
+                $"Things/Buildings/ZPM-{powerState}", ShaderDatabase.DefaultShader, new Vector2(1, 2), Color.white,
+                Color.white, new GraphicData(), 0, null);
+        #else
+            GraphicRequest request = new GraphicRequest(Type.GetType("Graphic_Single"),
+                $"Things/Buildings/ZPM-{powerState}", ShaderDatabase.DefaultShader, new Vector2(1, 2), Color.white,
+                Color.white, new GraphicData(), 0, null, null);
+        #endif
+            graphic.Init(request);
+            chargeGraphics.Add(powerState, graphic);
         }
-
-        public override void TickRare()
-        {
-            if (this.power.PowerNet.CurrentEnergyGainRate() > 0.01f)
-            {
-                // Charge using all the excess energy on the grid.
-                darkEnergyReserve += 100;
-            }
-            
-            if (darkEnergyReserve > maxDarkEnergy)
-            {
-                darkEnergyReserve = maxDarkEnergy;
-            }
-            
-            if (this.power.StoredEnergyPct < 0.75f && darkEnergyReserve >= 1000)
-            {
-                this.power.AddEnergy(1000f);
-                darkEnergyReserve -= 1000;
-            }
-            
-            // Log.Error("Current Energy Gain Rate: " + this.power.PowerNet.CurrentEnergyGainRate());
-            // Log.Error("Stored Energy: " + this.power.StoredEnergy);
-            base.TickRare();
-        }
-
-        #endregion
-
-        #region Graphics-text
-
-        public override Graphic Graphic
-        {
-            get
-            {
-                // For when it's minified or in a trade ship.
-                if (this.power == null)
-                {
-                    return base.DefaultGraphic;
-                }
-                // var chargePercent = (int) ((float) this.currentCapacitorCharge / (float) this.maxCapacitorCharge) * 100;
-                var chargePercent = (int) (this.power.StoredEnergyPct * 100);
-                if (chargePercent <= 10)
-                {
-                    return Building_ZPM.chargeGraphics["Depleted"];
-                }
-                else if (chargePercent <= 25)
-                {
-                    return Building_ZPM.chargeGraphics["25%"];
-                }
-                else if (chargePercent <= 50)
-                {
-                    return Building_ZPM.chargeGraphics["50%"];
-                }
-                else if (chargePercent <= 75)
-                {
-                    return Building_ZPM.chargeGraphics["75%"];
-                }
-                else
-                {
-                    return Building_ZPM.chargeGraphics["Full"];
-                }
-            }
-        }
-        public override string GetInspectString()
-        {
-            return base.GetInspectString() + "\n"
-                + "Dark Energy Reserve: " + this.darkEnergyReserve + " / " + this.maxDarkEnergy;
-        }
-
-        #endregion
     }
+
+    #region Override
+
+    public override void SpawnSetup(Map map, bool respawningAfterLoad)
+    {
+        this.currentMap = map;
+        base.SpawnSetup(map, respawningAfterLoad);
+
+        this.power = base.GetComp<CompPowerBattery>();
+        this.maxDarkEnergy = (int) Math.Ceiling(this.power.Props.storedEnergyMax * 1.25);
+    }
+
+    public override void TickRare()
+    {
+        if (this.power.PowerNet.CurrentEnergyGainRate() > 0.01f)
+        {
+            // Charge using all the excess energy on the grid.
+            darkEnergyReserve += 100;
+        }
+
+        if (darkEnergyReserve > maxDarkEnergy)
+        {
+            darkEnergyReserve = maxDarkEnergy;
+        }
+
+        if (this.power.StoredEnergyPct < 0.75f && darkEnergyReserve >= 1000)
+        {
+            this.power.AddEnergy(1000f);
+            darkEnergyReserve -= 1000;
+        }
+
+        // Log.Error("Current Energy Gain Rate: " + this.power.PowerNet.CurrentEnergyGainRate());
+        // Log.Error("Stored Energy: " + this.power.StoredEnergy);
+        base.TickRare();
+    }
+
+    #endregion
+
+    #region Graphics-text
+
+    public override Graphic Graphic
+    {
+        get
+        {
+            // For when it's minified or in a trade ship.
+            if (this.power == null)
+            {
+                return base.DefaultGraphic;
+            }
+
+            // var chargePercent = (int) ((float) this.currentCapacitorCharge / (float) this.maxCapacitorCharge) * 100;
+            var chargePercent = (int) (this.power.StoredEnergyPct * 100);
+            if (chargePercent <= 10)
+            {
+                return Building_ZPM.chargeGraphics["Depleted"];
+            }
+            else if (chargePercent <= 25)
+            {
+                return Building_ZPM.chargeGraphics["25%"];
+            }
+            else if (chargePercent <= 50)
+            {
+                return Building_ZPM.chargeGraphics["50%"];
+            }
+            else if (chargePercent <= 75)
+            {
+                return Building_ZPM.chargeGraphics["75%"];
+            }
+            else
+            {
+                return Building_ZPM.chargeGraphics["Full"];
+            }
+        }
+    }
+    public override string GetInspectString()
+    {
+        return base.GetInspectString() + "\n"
+            + "Dark Energy Reserve: " + this.darkEnergyReserve + " / " + this.maxDarkEnergy;
+    }
+
+    #endregion
 }
+
